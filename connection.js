113a3ec423e937e54b5f4b5b25705c96d9981e09
kajal <you@example.com>
2020-06-04 14:02:21 +0530

    updated

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
     connection: {
         host : '127.0.0.1',
         user : 'root',
-        password : 'kajal****',
+        password : 'Kajal@123',
         database : 'blog_Application'
     }
 });
